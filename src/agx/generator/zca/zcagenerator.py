4558a774a653861b22182c40e60571de0bacae34
Philipp Auersperg-Castell <phil@bluedynamics.com>
2011-10-02 19:43:19 +0200

    support for interfaces

--- src/agx/generator/zca/zcagenerator.py
+++ src/agx/generator/zca/zcagenerator.py
@@ -1,5 +1,7 @@
 import os
 
+from zope.component.interfaces import ComponentLookupError
+
 from agx.core import (
     handler,
     Scope,
@@ -7,7 +9,14 @@ from agx.core import (
     token
 )
 from agx.core.util import read_target_node, dotted_path
+from agx.generator.pyegg.connectors import base_name
+
+from node.ext.python.utils import Imports
 from node.ext.uml.interfaces import IInterface,IInterfaceRealization,IDependency
+from node.ext.python.interfaces import IBlock, IDocstring
+
+
+from node.ext import python
 from node.ext.python.interfaces import IFunction
 from node.ext.python.nodes import (
     Function,
@@ -25,17 +34,41 @@ from node.ext.zcml import ZCMLFile
 from node.ext.zcml import SimpleDirective
 from node.ext.zcml import ComplexDirective
 
+from agx.generator.pyegg.utils import (
+    templatepath,
+    set_copyright,
+)
+
 
 registerScope('zcainterface', 'uml2fs', [IInterface], Scope)
 registerScope('zcarealize', 'uml2fs', [IInterfaceRealization], Scope)
 registerScope('zcaadapts', 'uml2fs', None, AdaptsScope)
 
-@handler('zcainterface', 'uml2fs', 'zcagenerator', 'zcainterface')
+#@handler('zcainterface', 'uml2fs', 'zcagenerator', 'zcainterface')
+@handler('zcainterface', 'uml2fs', 'hierarchygenerator', 'zcainterface')
 def zcainterface(self, source, target):
     """Create zope interface.
     """
-    #print 'zcainterface'
-    #print source, target
+    if source.stereotype('pyegg:stub') is not None:
+        return
+    name = source.name
+    module = target.anchor
+    imp = Imports(module)
+    imp.set('zope.interface', [['Interface', None]])
+
+    
+    set_copyright(source, module)
+    if module.classes(name):
+        class_ = module.classes(name)[0]
+        if 'Interface' not in class_.bases:
+            class_.bases.append('Interface')
+        target.finalize(source, class_)
+        return
+    class_ = python.Class(name)
+    module[name] = class_
+
+
+    target.finalize(source, class_)
 
 registerScope('zcautility', 'uml2fs', None, UtilityScope)
 
@@ -78,12 +111,9 @@ def zcaadapter_zcml(self, source, target):
     #print 'zcaadapter'
     #print source, target
 
-@handler('zcarealize', 'uml2fs','zcagenerator','zcaadapter')
-def zcarealize(self, source, target):
-    pass
-
 @handler('zcaadapts', 'uml2fs','zcagenerator','zcaadapts')
 def zcaadapts(self, source, target):
+#    import pdb;pdb.set_trace()
     tok=token(str(source.client.uuid),True)
     pack=source.parent
     targetdir=read_target_node(pack, target.target)
@@ -107,3 +137,52 @@ def zcaadapts(self, source, target):
     adapts.attrs['name'] = name
     adapts.attrs['factory'] = factory
 
+@handler('zcarealize', 'uml2fs','semanticsgenerator','zcarealize')
+def zcarealize(self, source, target):
+    klass=source.implementingClassifier
+    ifacename=source.contract.name
+    targetclass=read_target_node(klass,target.target)
+    tok=token(str(klass.uuid),True,realizes=[])
+    tok.realizes.append(ifacename)
+    targetinterface=read_target_node(source.contract,target.target)
+    
+    #import the interface
+    imp = Imports(targetclass.__parent__)
+    imp.set(base_name(targetinterface), [[targetinterface.classname, None]])
+
+
+@handler('zcarealize_finalize', 'uml2fs','zcagenerator','pyclass')
+def zcarealize_finalize(self, source, target):
+#   get the collected realizes 
+    klass=source
+    try:
+        tok=token(str(klass.uuid),False)
+        ifacenames=tok.realizes
+        targetclass=read_target_node(klass,target.target)
+        imptext='implements(%s)' % ','.join(ifacenames)
+        docstrings=targetclass.filteredvalues(IDocstring)
+
+        #delete all implements stmts
+        try:
+            blocks=targetclass.filteredvalues(IBlock)
+            for b in blocks:
+                if b.text.strip().startswith('implements('):
+                    del targetclass[str(b.uuid)]
+            #XXX: soll repased werden
+        except KeyError:
+            print 'error during delete'
+            pass
+        
+        block=Block(imptext)
+        block.__name__='implements'
+        targetclass.insertfirst(block)
+
+        if docstrings:
+            imp=targetclass.detach('implements')
+            targetclass.insertafter(imp,docstrings[0])
+
+        # vors __init__
+    except ComponentLookupError:
+        #keine realize parents vorhanden
+        pass
+    
